08c444d0e6d30bd3a1048a1ef53288dcecb553ba
Peizhi Zheng <pzheng16@asu.edu>
2020-09-03 14:51:21 -0700

    callbacks api example

--- Callbacks_Promises_Async_Await.js
+++ Callbacks_Promises_Async_Await.js
@@ -42,3 +42,79 @@ $('#btn').on('click',()=>(
 
 //two usages for callback function:
 //1.passing one values return another value
+//2.delay exectuaction until a certain time, particular event happened.
+
+
+function getUser(id, onSuccess, onFailure) {
+  $.getJSON({
+    url: `https://api.github.com/users/${id}`,
+    success: onSuccess,
+    error: onFailure,
+  });
+}
+
+//giving your number to a resturant is like giving a callback function to a third party services.
+//once the callback function in the third party hand, you kind lost lots of controls. 
+//instead of giving your number to a resturant, they give you a resturant buzzer
+
+//3 states: pending, fulfilled and rejected
+
+//promises exist makes asynchronous request more managable
+
+//how to create a promise?
+//how to change status of the promis?
+const promise = new Promise((resolve,reject)=>{
+    resolve() //fullfiled
+    reject()// rejected
+})
+
+//chainging
+
+import $ from "jquery";
+
+const id = "tylermcginnis";
+function showError(e) {
+  console.warn("Error", e);
+}
+function getLocationURL([city, state]) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${city},${state}&APPID=7c748e66ec4489f390a888a83eb4a0f4`;
+}
+
+function updateUI(info) {
+  $("#app").text(JSON.stringify(info));
+}
+
+function getUser(id, onSuccess, onFailure) {
+  $.getJSON({
+    url: `https://api.github.com/users/${id}`,
+    success: onSuccess,
+    error: onFailure,
+  });
+}
+function getWeather(user, onSuccess, onFailure) {
+  $.getJSON({
+    url: getLocationURL(user.location.split(",")),
+    success: onSuccess,
+    error: onFailure,
+  });
+}
+
+$("#btn").on("click", () => {
+  getUser(
+    "hibiken",
+    (user) => {
+      getWeather(
+        user,
+        (weather) => {
+          updateUI({
+            user,
+            weather: weather.city,
+          });
+        },
+        showError
+      );
+    },
+    showError
+  );
+});
+
